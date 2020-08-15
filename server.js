ea2538e6ef1a513a71bb5c362956c03318daa1c5
Glitch (bcrypt-hashes-uaaeu) <none>
2020-08-15 13:52:21 +0000

    Hash and Compare Passwords Synchronously

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash)=>{
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
 
-
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 //END_SYNC
 
 
